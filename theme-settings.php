3afe12130712736d820fafb55c9ed09320a625e8
Lacey Sanderson <laceyannesanderson@gmail.com>
2024-02-15 17:05:13 -0600

    Update the theme settings to override the olivero settings with colours we are interested in.

--- theme-settings.php
+++ theme-settings.php
@@ -12,16 +12,44 @@ use Drupal\Core\Form\FormState;
  */
 function trpcultivatetheme_form_system_theme_settings_alter(array &$form, FormState $form_state): void {
 
-  $form['trpcultivatetheme'] = [
-    '#type' => 'details',
-    '#title' => t('Tripal Cultivate Base Theme'),
-    '#open' => TRUE,
+  $color_config = [
+    'colors' => [
+      'base_primary_color' => 'Primary base color',
+    ],
+    'schemes' => [
+      'default' => [
+        'label' => 'Tripal Cultivate Default',
+        'colors' => [
+          'base_primary_color' => '#3C8700',
+        ],
+      ],
+      'grasslandgenomics' => [
+        'label' => 'Grassland Genomics',
+        'colors' => [
+          'base_primary_color' => '#2c6743',
+        ],
+      ],
+      'activatingcrops' => [
+        'label' => 'Activating Crops',
+        'colors' => [
+          'base_primary_color' => '#4e7e46',
+        ],
+      ],
+      'knowpulse' => [
+        'label' => 'KnowPulse',
+        'colors' => [
+          'base_primary_color' => '#2F4153',
+        ],
+      ],
+    ],
   ];
 
-  $form['trpcultivatetheme']['example'] = [
-    '#type' => 'textfield',
-    '#title' => t('Example'),
-    '#default_value' => theme_get_setting('example'),
-  ];
+  $form['#attached']['drupalSettings']['olivero']['colorSchemes'] = $color_config['schemes'];
 
+  // Update the drop down with the colour schemes defined above.
+  $options = [];
+  foreach ($color_config['schemes'] as $key => $details) {
+    $options[$key] = $details['label'];
+  }
+  $form['olivero_settings']['olivero_utilities']['olivero_color_scheme']['color_scheme']['#options'] = $options;
 }
